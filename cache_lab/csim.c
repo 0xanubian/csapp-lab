064637b10205451271e60ea3c0cd3a74aec7b000
Anshuman22 <kumaranshuman2273@gmail.com>
2026-08-05 16:38:57 +0530

    all initialisation and parsing is done now its time for core cache simulation

--- cache_lab/csim.c
+++ cache_lab/csim.c
@@ -1,13 +1,114 @@
-#define _POSIX_C_SOURCE 200809L
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-
 #include "cachelab.h"
 #include "csim.h"
 
 struct cache_system_t g_cache_system = {0};
+
+/*
+ * load function
+ */
+void load(struct operation_t operation);
+
+/*
+ * store function
+ */
+void store(struct operation_t operation);
+
+/*
+ * modify function
+ */
+void modify(struct operation_t operation);
+
+//retrives metadata from the mem addr of operation and sets the metadata struct
+void retrieve_metadata(struct operation_t *operation)
+{
+    unsigned long long addr = operation->mem_addr;
+
+    unsigned long long block_offset = addr & (g_cache_system.block_size - 1);
+    addr = addr >> g_cache_system.nblock_offset_bits;
+    unsigned long long set_index = addr & (g_cache_system.nsets - 1);
+    unsigned long long tag = addr >> g_cache_system.nset_index_bit;
+
+    operation->metadata.block_offset = block_offset;
+    operation->metadata.set_index = set_index;
+    operation->metadata.tag = tag;
+
+    return;
+}
+
+//parse a line from trace file to fill the operation_t struct
+struct operation_t parse_line(char *line)
+{
+    char op;
+    unsigned long long mem_addr;
+    int size;
+
+    if (sscanf(line, " %c %llx,%d", &op, &mem_addr, &size) != 3)
+        perror("Failure while parsing lines from the trace file");
+
+    struct operation_t operation = {{0}, mem_addr, size, op};
+    return operation;
+}
+
+/*
+ * start cache simulation by reading all the lines from trace file and parsing
+ * it. 
+ */
+void start_simulation(void)
+{
+    g_cache_system.file = fopen(g_cache_system.trace_file, "r");
+
+    FILE *file = g_cache_system.file;
+    if (file == NULL) {
+        perror("Error opening the trace file");
+        exit(-1);
+    }
+
+    char *line = NULL;
+    size_t size = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &size, file)) != -1) {
+        struct operation_t operation = parse_line(line);
+        //printf("op: %c, addr: %llx, size: %d\n", operation.op, operation.mem_addr, operation.size);
+        retrieve_metadata(&operation);
+
+        char op = operation.op;
+        switch (op) {
+            case 'L':
+                load(operation);
+                break;
+            case 'S':
+                store(operation);
+                break;
+            case 'M':
+                modify(operation);
+                break;
+            default:
+                fprintf(stderr, "invalid operation: %c\n", op);
+                exit(-1);
+        }
+
+    }
+}
+
+/* initialise the cache system: allocates memory for sets and initialise the 
+ * nines to null
+ */
+void init_cache(void)
+{
+    unsigned nsets = g_cache_system.nsets;
+    
+    g_cache_system.sets = calloc(nsets, sizeof(struct cache_set_t));
+
+    struct cache_set_t *sets = g_cache_system.sets;
+    for (int i = 0; i < nsets; i++) {
+        sets[i].line = NULL;
+        sets[i].end = NULL;
+        sets[i].ncachelines_allocated = 0;
+    }
+
+    return;
+}
 
 // power function for int data types
 int ipow(int base, unsigned exp)
@@ -91,12 +192,14 @@ void parse_args(int argc, char **argv)
     g_cache_system.is_verbose = is_verbose;
 
     //for debug use. it prints all the cli args parsed
-    printf("s: %d, E: %d, b: %d, t: %s, v: %d\n", s, E, b, trace_file, is_verbose);
+    //printf("s: %d, E: %d, b: %d, t: %s, v: %d\n", s, E, b, trace_file, is_verbose);
 }
 
 int main(int argc, char **argv, char **envp)
 {
     parse_args(argc, argv);
+    init_cache();
+    start_simulation();
 
     printSummary(0, 0, 0);
     return 0;
--- cache_lab/csim.h
+++ cache_lab/csim.h
@@ -1,17 +1,40 @@
+#define _POSIX_C_SOURCE 200809L
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+struct metadata_t {
+    unsigned long long block_offset;
+    unsigned long long set_index;
+    unsigned long long tag;
+};
+
+struct operation_t {
+    struct metadata_t metadata;
+    unsigned long long mem_addr;
+    int size;
+    char op;
+};
+
 struct cache_line_t {
     struct cache_line_t *next;  // a pointer to the next line
-    unsigned char *cache_block; // an array of bytes for mem block in cache
+    struct cache_line_t *prev;  // a pointer to the previous line
+    //unsigned char *cache_block; // an array of bytes for mem block in cache
     int valid;                  // valid bit
     int tag;                    // tag to match with the tag from memory addr
 };
 
 struct cache_set_t {
-    struct cache_line_t *line;  // a pointer to the linked list of cache lines
-    unsigned cache_index;       // index of current set
+    struct cache_line_t *line;  // a pointer to the head of linked list of cache lines
+    struct cache_line_t *end;   // a pointer to the tail of linked list of cache lines
+    unsigned ncachelines_allocated;       // index of current set
 };
 
 struct cache_system_t {
     struct cache_set_t *sets;   // array of sets allocated dynamically
+    char *trace_file;
+    FILE *file;
 
     unsigned nsets;                  // 2^s
     unsigned nline;                  // E
@@ -20,11 +43,10 @@ struct cache_system_t {
     unsigned nblock_offset_bits;     // b
 
     int is_verbose;
-    char *trace_file;
 };
-static const char *missing_cli_args = "./csim-ref: Missing required command line argument\n";
+static const char *missing_cli_args = "./csim: Missing required command line argument\n";
 
-static const char *usage_message = "Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n"
+static const char *usage_message = "Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n"
                             "Options:\n"
                             "  -h         Print this help message.\n"
                             "  -v         Optional verbose flag.\n"
@@ -33,5 +55,5 @@ static const char *usage_message = "Usage: ./csim-ref [-hv] -s <num> -E <num> -b
                             "  -b <num>   Number of block offset bits.\n"
                             "  -t <file>  Trace file.\n"
                             "\nExamples:\n"
-                            "  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n"
-                            "  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace\n";
+                            "  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n"
+                            "  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n";
